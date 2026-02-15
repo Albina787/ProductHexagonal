511f196d5cbd0660359713fa20a5ab621dd6b26d
Albina787 <c.hasan.albina@student.uzhnu.edu.ua>
2026-02-15 19:55:16 +0200

    Create Product.cs

---
+++ Domain/Product.cs
@@ -0,0 +1,9 @@
+namespace ProductHexagonal.Domain;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public decimal Price { get; set; }
+    public string Photo { get; set; } = "";
+}
